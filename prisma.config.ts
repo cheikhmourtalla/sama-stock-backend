261228d949a86450b323c112376e862a78951caf
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-05-18 22:33:17 +0000

    prisma0

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,12 +4,12 @@ import { env } from "./src/config/env/env.js";
 
 console.log(env.db.url)
 
-// export default defineConfig({
-//   schema: "./src/prisma/schema.prisma",
-//   migrations: {
-//     path: "./src/prisma/migrations",
-//   },
-//   datasource: {
-//     url: env.db.url ,
-//   },
-// });
+export default defineConfig({
+  schema: "./prisma/schema.prisma",
+  migrations: {
+    path: "./prisma/migrations",
+  },
+  datasource: {
+    url: env.db.url ,
+  },
+});
