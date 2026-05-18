c1c0b0be08776c9f5d3688b48234575ccb87b1a4
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-05-18 21:57:08 +0000

    user generated client_!

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,9 +1,7 @@
 import "dotenv/config";
-import { PrismaClient } from "../prisma/generated/prisma/client.js"; // path depends on your output setting
+import { PrismaClient } from "../prisma/generated/prisma/client"; // path depends on your output setting
 import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "./env/env";
-
-PrismaClient;
 
 const adapter = new PrismaPg({
   connectionString: env.db.url,
